862218cc1bbd56edce810fb3a2b2205f365a1543
ErishaStha <erishaastha@gmail.com>
2026-03-24 17:55:40 -0700

    Update extract.py

--- script/extract.py
+++ script/extract.py
@@ -1,10 +1,1 @@
-import pandas as pd
-
-# Load dataset
-df = pd.read_csv('C:/Users/erish/Downloads/vehicle-etl-pipeline/Data/project-folderdataraw_data.csv')
-
-# Preview data
-print(df.head())
-
-# Check structure
-print(df.info())
+C:\Users\erish\Documents\project-folderdataraw_data.csv
