951fd785e48eb11eafbd2ea59d0f649aac08902b
ErishaStha <erishaastha@gmail.com>
2026-03-24 19:29:49 -0700

    Update extract.py

--- script/extract.py
+++ script/extract.py
@@ -1,1 +1,10 @@
-C:\Users\erish\Documents\project-folderdataraw_data.csv
+import pandas as pd
+
+# Load dataset
+df = pd.read_csv('project-folderdataraw_data.csv')
+
+# Preview data
+print(df.head())
+
+# Check structure
+print(df.info())
